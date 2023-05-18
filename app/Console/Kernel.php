dbb7712e0834256de7f14383c3047c5867ce4699
swebswu <pili.sebastianbach@gmail.com>
2023-05-18 13:13:42 +0800

    checkpoint

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,12 +7,16 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        "App\Console\Commands\DBBackup"
+    ];
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('db:backup')->daily();
     }
 
     /**
